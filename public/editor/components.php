ee2d9ac1f46c16263ef2b5df373689c6004dfaec
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-07-13 14:55:36 +0200

    - Ajout de quelques composants de démonstration

--- public/editor/components.php
+++ public/editor/components.php
@@ -24,17 +24,60 @@ class Component {
 }
 
 $components = [
-    (new Component("event.acme.HelloWorld", 2))
+    // event
+    (new Component("event.HelloWorld", 2))
         ->addOuput("text")
     ,
+    // system
     (new Component("system.Print", 2))
         ->addInput("value")
         ->addOuput("value")
     ,
+    (new Component("system.Exit", 2))
+        ->addInput("value")
+    ,
+    // math
     (new Component("math.Addition", 2))
         ->addInput("a", "float")
         ->addInput("b", "float")
+        ->addOuput("a+b")
+    ,
+    (new Component("math.Substraction", 2))
+        ->addInput("a", "float")
+        ->addInput("b", "float")
+        ->addOuput("a-b")
+    ,
+    (new Component("math.Multiplication", 2))
+        ->addInput("a", "float")
+        ->addInput("b", "float")
+        ->addOuput("a*b")
+    ,
+    (new Component("math.Division", 2))
+        ->addInput("a", "float")
+        ->addInput("b", "float")
+        ->addOuput("a/b")
+    ,
+    // api.heimdall
+    (new Component("api.heimdall.Execute", 2))
+        ->addInput("switch", "string")
+        ->addInput("command", "string")
         ->addOuput("result")
+    ,
+    // api.oceane
+    (new Component("api.oceane.Ticket", 2))
+        ->addInput("ticket_id", "string")
+        ->addOuput("ticket", "dict")
+    ,
+    (new Component("api.oceane.RechercheRessource", 2))
+        ->addInput("idt11", "string")
+        ->addInput("idt21", "string")
+        ->addInput("idt31", "string")
+        ->addOuput("ressource", "dict")
+    ,
+    (new Component("api.oceane.ImpactClient", 2))
+        ->addInput("ticket_id", "string")
+        ->addOuput("impact_technique", "string")
+        ->addOuput("impact_client", "string")
     ,
 
 ];
