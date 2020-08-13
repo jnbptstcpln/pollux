b7ade92e12066eeb23b69cc5f0129532518d1bdf
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-08-13 20:47:24 +0200

    - Mise en place du module Label (avec sa config) - Ajout du lien vers la base de données

--- src/Application.php
+++ src/Application.php
@@ -11,14 +11,18 @@ namespace CPLN;
 
 use CPLN\Services\APIMiddleware;
 use CPLN\Services\Assets;
+use CPLN\Services\Label;
+use Plexus\Service\DatabaseManager;
 use Plexus\Service\Renderer\TwigRenderer;
 use Plexus\Utils\Path;
 
 class Application extends \Plexus\Application {
 
     protected function registerServices() {
+        DatabaseManager::fromRuntime($this);
         $twig = TwigRenderer::fromRuntime($this, Path::build($this->root_path, "src", "templates"));
         $assets = Assets::fromRuntime($this);
+        $label = Label::fromRuntime($this);
         // Call APIMiddleware to add a new middleware to the router before adding modules' routes
         $apiMiddleware = APIMiddleware::fromRuntime($this);
     }
