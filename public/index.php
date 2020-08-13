bf8e1405d5ee35364078a37a47f8803cfc2f78a7
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-08-13 20:43:23 +0200

    Précision de la timezone

--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@ session_start();
 
 error_reporting(E_ERROR | E_PARSE);
 
+date_default_timezone_set("Europe/Paris");
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new \CPLN\Application(__DIR__ . '/..');
