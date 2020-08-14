cc6d3d0198c2ce2dc5c1530378cb1c91e94b26d2
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-08-14 15:47:27 +0200

    Ajout de la description des champs

--- src/assets/editor/form.js
+++ src/assets/editor/form.js
@@ -5,15 +5,31 @@ Form.create = function (html) {
 Form.fieldset = function (name) {
     return $.DOM.create("fieldset").html("<legend>{0}</legend>".format(name))
 }
-Form.input = function (name, label, type, value, oninput) {
-    var div = $.DOM.create("div").addClass("field").html("<label>{0}</label><input name='{1}' type='{2}' value='{3}'>".format(label, name, type, value));
+Form.input = function (name, label, type, value, oninput, help_text) {
+    help_text = help_text ? "<p class='text-help'>{0}</p>".format(help_text) : "";
+    var div = $.DOM.create("div").addClass("field").html("<label>{0}</label>{4}<input name='{1}' type='{2}' value='{3}'>".format(label, name, type, value, help_text));
     div.find('input').on('input', function(e) {
         oninput(e.element.attr('name'), e.element.get('value'));
     });
     return div;
 }
-Form.textarea = function (name, label, value, oninput) {
-    var div = $.DOM.create("div").addClass("field").html("<label>{0}</label><textarea name='{1}'>{2}</textarea>".format(label, name, value));
+Form.textarea = function (name, label, value, oninput, help_text) {
+    help_text = help_text ? "<p class='text-help'>{0}</p>".format(help_text) : "";
+    var div = $.DOM.create("div").addClass("field").html("<label>{0}</label>{3}<textarea name='{1}'>{2}</textarea>".format(label, name, value, help_text));
+    div.find('textarea').on('input', function(e) {
+        oninput(e.element.attr('name'), e.element.get('value'));
+    });
+    return div;
+}
+Form.select = function (name, label, options, value, oninput, help_text) {
+
+    var options_html = "";
+    for (var i in options) {
+        options_html += "<option value='{0}' {2}>{1}</option>".format(i, options[i], value == i ? 'selected' : '');
+    }
+
+    help_text = help_text ? "<p class='text-help'>{0}</p>".format(help_text) : "";
+    var div = $.DOM.create("div").addClass("field").html("<label>{0}</label>{3}<select name='{1}'>{2}</select>".format(label, name, value, help_text));
     div.find('textarea').on('input', function(e) {
         oninput(e.element.attr('name'), e.element.get('value'));
     });
